4c0b0ddde82e30505c8e277e474a5fce4fd255a5
Henry Calderon <theceintiant@gmail.com>
2020-02-04 12:16:08 -0800

    Wanting to save stuff before major changes are made.

---
+++ subscriptionBoxTutorial/View/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  subscriptionBoxTutorial
+//
+//  Created by Henry Calderon on 2/4/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
