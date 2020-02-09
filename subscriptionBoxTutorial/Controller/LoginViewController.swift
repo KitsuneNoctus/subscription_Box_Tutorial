280dfc5a852d4ebe803ac87bc9c98f2390186df7
Henry Calderon <theceintiant@gmail.com>
2020-02-08 20:36:11 -0800

    Making the stuff ready for use in login view/

--- subscriptionBoxTutorial/Controller/LoginViewController.swift
+++ subscriptionBoxTutorial/Controller/LoginViewController.swift
@@ -10,6 +10,62 @@ import UIKit
 
 class LoginViewController: UIViewController {
     //Once again testing
+    
+//    var loginTitle: UILabel!
+//    var usernameTextField: UITextField!
+//    var passwordTextField: UITextField!
+    
+    let loginTitle: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = UIFont(name: "Helvetica", size: 20)
+        label.textColor = UIColor(white: 1.0, alpha: 0.8)
+        label.text = "PetBox"
+        return label
+    }()
+    
+    var usernameTextField: UITextField = {
+       let usernameTextField = UITextField()
+        usernameTextField.text = "username"
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        return usernameTextField
+    }()
+    
+    var passwordTextField: UITextField = {
+        let passwordTextField = UITextField()
+        passwordTextField.text = "password"
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        return passwordTextField
+    }()
+    
+    
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(UIColor.darkGray, for: .normal)
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = true
+        button.backgroundColor = UIColor(white: 1.0, alpha: 0.9)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
