87cabc1f1375899f65bb58333825eb8424a13047
Henry Calderon <theceintiant@gmail.com>
2020-02-11 11:51:10 -0800

    Adding Model

---
+++ subscriptionBoxTutorial/Model/File.swift
@@ -0,0 +1,41 @@
+//
+//  File.swift
+//  subscriptionBoxTutorial
+//
+//  Created by Henry Calderon on 2/11/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import Foundation
+
+//MARK: The Main Settings
+struct Item {
+    let name: String
+    let image: String
+}
+
+struct Box {
+    let date: String
+    let items: [Item]
+}
+
+struct Category {
+    let name: String
+}
+//MARK: My Own Additions
+// My own custom model data
+enum Themes {
+    case Halloween
+    case Christmas
+    case Thanksgiving
+    case Valentines
+}
+
+struct Themed {
+    let isThemed: Bool
+    let theTheme: [Themes]
+}
+//MARK: The User
+struct User {
+    
+}
