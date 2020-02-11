30f114167cda8a9f772eb5f09faa8f44891c0583
Henry Calderon <theceintiant@gmail.com>
2020-02-11 12:51:38 -0800

    Holy ____ a lot changed
    There was fixing the login view and the Home view. Trying to fix the changes between pages but not getting it. Trying to fix what ruined my screens now as well.

---
+++ subscriptionBoxTutorial/navigationController.swift
@@ -0,0 +1,9 @@
+//
+//  navigationController.swift
+//  subscriptionBoxTutorial
+//
+//  Created by Henry Calderon on 2/11/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import Foundation
